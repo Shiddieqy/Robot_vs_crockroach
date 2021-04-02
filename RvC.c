8f245bbc6199e58fa89753095eb997f08930bb1e
Shiddieqy <shabriashiddieqy@gmail.com>
2021-04-02 19:31:58 +0700

    Add comment

--- RvC.c
+++ RvC.c
@@ -1,8 +1,13 @@
+//Robot Vs Crockroach
+//program dari sebuah robot yang ditugaskan untuk membasmi kecoak mutan
+
+//import library
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+//deklarasi fungsi
 void forwards(int* xp,int* yp);
 void backward(int* xp,int* yp);
 void right(int* xp,int* yp);
@@ -14,7 +19,9 @@ float readrange(int xp,int yp,int xc, int yc);
 void stopprogram(int *ccount);
 void cmove(int* xc, int* xp, int* yc,int* yp, float range);
 
+
 int main() {
+    //deklarasi variabel
     int xp = 0;
     int yp = 0;
     int xc,yc;
@@ -24,27 +31,41 @@ int main() {
     int inp;
     int ccount=0;
 
+    //loop utama
     while(HPp>=0){
+
+        //mengecek apakah kecoak sudah mati atau masih ada di arena
         if (HPc == 0){
+            //jika mati akan dimunculkan kecoak baru
             spawnC(&xc,&yc);
+            //lokasi kecoak tidak boleh sama dengan lokasi robot
             while (xc==xp && yp==yc){
                 spawnC(&xc,&yc);
             }
+
+            //mengatur Hit Point dari kecoak
             HPc=10;
             printf("\nNew Crockroach spawned");
         }
 
+        //membaca jarak dengan kecoak dan menampilkan informasi jarak, HP, dan posisi.
         range = readrange(xp,yp,xc,yc);
         printf("\nJarak dengan kecoak %.2f",range);
         printf("\nHP : %d",HPp);
         printf("\nposisi : (%d,%d)",xp,yp);
         printf("\nposisi kecoa : (%d,%d)",xc,yc);
+
+        //Program berhenti jika robot mati
         if(HPp==0){
             printf("\nKamu mati :((( \nJumlah kecoa yang terbunuh %d",ccount);
             break;
         }
+
+        //Menampikan instruksi untuk input
         printf("\nPilih langkah : \n1 untuk maju\n2 untuk mundur\n3 untuk ke kanan\n4 untuk ke kiri\n5 untuk menembak\n6 untuk hentikan program\n");
         scanf("%d", &inp);
+
+        //Menjaankan prosedur sesuai input yang diterima
         if (inp==1){
             forwards(&xp,&yp);
         }
@@ -64,6 +85,8 @@ int main() {
             stopprogram(&ccount);
             break;
         }
+
+        //Kecoak akan bergerak dan menembak tiap loop berjalan
         cmove(&xc,&xp,&yc,&yp,readrange(xp,yp,xc,yc));
         cshoot(range,&HPp);
     }
@@ -71,6 +94,7 @@ int main() {
 
 }
 
+//Fungsi untuk maju, karena area dibatasi hanya 20 x 20. Robot tidak bisa maju lagi ketika yp=20
 void forwards(int* xp,int* yp){
     if (*yp==20){
         printf("\nCant move forward!!!!");
@@ -80,6 +104,8 @@ void forwards(int* xp,int* yp){
     }
 
 }
+
+//Fungsi untuk mundur, karena area dibatasi hanya 20 x 20. Robot tidak bisa mundur lagi ketika yp=0
 void backward(int* xp,int* yp){
     if (*yp == 0){
         printf("\nCant move backward!!!");
@@ -89,6 +115,8 @@ void backward(int* xp,int* yp){
     }
 
 }
+
+//Fungsi untuk ke kanan,karena area dibatasi hanya 20 x 20. Robot tidak ke kanan lagi ketika xp=20
 void right(int* xp,int* yp){
     if (*xp == 20){
         printf("\nCant move right!!!");
@@ -97,6 +125,8 @@ void right(int* xp,int* yp){
         ++*xp;
     }
 }
+
+//Fungsi untuk ke kiri,karena area dibatasi hanya 20 x 20. Robot tidak ke kiri lagi ketika xp=0
 void left(int* xp,int* yp){
     if (*xp == 0){
         printf("\nCant move left!!!");
@@ -105,10 +135,14 @@ void left(int* xp,int* yp){
         --*xp;
     }
 }
+
+//Fungsi untuk menembak
 void shoot(float range,int* HPc,int* ccount){
+    //Hanya bisa menembak jika kecoak dalam jangkauan
     if (range>10){
         printf("Cant shoot!!");
     }
+    //Jika dalam jangkauan, kecoak akan tertembak dan langsung mati
     else {
         *HPc = 0;
         ++*ccount;
@@ -117,7 +151,10 @@ void shoot(float range,int* HPc,int* ccount){
     }
 
 }
+
+//Fungsi menembak untuk kecoak
 void cshoot(float range,int* HPp){
+    //kecoak menembak hanya ketika dalam jangkauan
     if (range<15){
         *HPp-=5;
         printf("\nYou are shot");
@@ -125,22 +162,31 @@ void cshoot(float range,int* HPp){
     }
 }
 
+//Fungsi untuk memunculkan kecoak
 void spawnC(int* xc, int* yc){
+    //Perlu dilakukan refresh seed agar selalu didapat angka random baru
+    /*rand adalah fungsi untuk memanggil integer secara acak, agar 
+    didapat integer dalam range 0-20 dilakukan operasi modulus terhadap 20*/
     srand ( time(0) );
     *xc = rand()%20;
     *yc = rand()%20;
 }
 
+//Fungsi untuk membaca jarak antara player (robot) dengan kecoak
 float readrange(int xp,int yp,int xc, int yc){
     int x = xp-xc;
     int y = yp-yc;
     return sqrt(x*x + y*y);
 }
 
+//Fungsi ketika user memilih menghentikan program
+//Akan dimunculkan jumlah kecoak yang terbunuh
 void stopprogram (int* ccount){
     printf("\n Program has been stopped\n Cockroach killed : (%d)",ccount);
 }
 
+//Fungsi bergerak untuk kecoak
+//Kecoak akan berusaha mendekati robot ketika robot berada di luar jangkauannya
 void cmove(int* xc, int* xp, int* yc,int* yp, float range){
     if (range > 15){
         if (abs(*xc-*xp)>abs(*yc-*yp)){
